a1e7f09de653a692b2e81efe15eff8c10c415572
luc <luc.mayor@gmail.com>
2024-01-19 11:05:34 -0600

    updating the repo

---
+++ check.py
@@ -0,0 +1,14 @@
+import asyncio
+from ossapi import Ossapi, Scope, UserLookupKey, GameMode, RankingType, OssapiAsync, Score
+
+client_id = 22219
+client_secret = "socY7zpCMAXyCGi9TFP3In8mt0gPBD5N7Nc96LIl"
+oss = Ossapi(client_id, client_secret)
+
+user = oss.user("alkalde", key=UserLookupKey.USERNAME)
+scores = oss.user_scores(user.id, include_fails=True, type="recent")
+print(scores)
+# needed: time, date, artist unicode, song unicode
+# time+date: created_at (datetime format)
+# artist: score.beatmapset.artist_unicode
+# song: score.beatmapset.title_unicode
